c07bf355eda0d4a330c5f5ddcf5d08c46c7667c5
planimir77 <34979738+planimir77@users.noreply.github.com>
2019-07-09 12:35:37 +0300

    Create Car.cs

---
+++ Defining Classes/Car Extension/Car.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+    public class Car
+    {
+        private string make;
+        private string model;
+        private int year;
+        private double fuelQuantity;
+        private double fuelConsumption;
+
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int Year { get; set; }
+
+        public double FuelQuantity { get; set; }
+
+        public double FuelConsumption { get; set; }
+
+        public Car ()
+        {
+            this.Make = make;
+            this.Model = model;
+            this.Year = year;
+            this.FuelQuantity = fuelQuantity;
+            this.FuelConsumption = FuelConsumption;
+        }
+
+        public void Drive(double distance)
+        {
+            //  fuelQuantity-distance*fuelConsumption
+            var result = this.FuelQuantity - (distance * this.FuelConsumption);
+            
+            if (result < 0)
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+            else
+            {
+                this.FuelQuantity -= distance * this.FuelConsumption;
+            }
+        }
+
+        public string WhoAmI()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Make: {this.Make}");
+            sb.AppendLine($"Model: {this.Model}");
+            sb.AppendLine($"Year: {this.Year}");
+            sb.AppendLine($"Fuel: { this.FuelQuantity:F2}L");
+            return sb.ToString().Trim();
+        }
+    }
+}
