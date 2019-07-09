7f8e98fbe893d919bffd1d6c1a5795ea9f6e20ff
planimir77 <34979738+planimir77@users.noreply.github.com>
2019-07-09 12:24:38 +0300

    Create Car.cs

---
+++ Defining Classes/08.Car_Salesman/Car.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DateModifier
+{
+    public class Car
+    {
+        private string model;
+        private Engine engine;
+        private int weight;
+        private string color ;
+
+        public string Model { get; set; }
+        public Engine Engine { get; set; }
+        public int Weight { get; set; }
+        public string Color { get; set; }
+        
+        public Car(string model, Engine engine)
+        {
+            this.Model = model;
+            this.Engine = engine;
+            this.Weight = 0;
+            this.Color = "n/a";
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"{this.Model}:");
+            sb.AppendLine($"  {this.Engine}");
+            if (this.Weight == 0)
+            {
+                sb.AppendLine("  Weight: n/a");
+            }
+            else
+            {
+                sb.AppendLine($"  Weight: {this.Weight}");
+            }
+            sb.AppendLine($"  Color: {this.Color}");
+            return sb.ToString().Trim();
+        }
+    }
+}
