9d957723c17b1c524fb8a7564f5ba8331c13e28a
planimir77 <34979738+planimir77@users.noreply.github.com>
2019-07-09 12:28:24 +0300

    Create Car.cs

---
+++ Defining Classes/10.SoftUniParking/Car.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftUniParking
+{
+    public class Car
+    {
+        // Make: string
+        // Model: string
+        // HorsePower: int
+        // RegistrationNumber: string
+        private string make;
+        private string model;
+        private int horsePower;
+        private string registrationNumber;
+
+        public Car(string make, string model, int horsePower, string registrationNumber )
+        {
+            this.Make = make;
+            this.Model = model;
+            this.HorsePower = horsePower;
+            this.RegistrationNumber = registrationNumber;
+        }
+
+        public string Make
+        {
+            get { return this.make; }
+            private set { this.make = value; }
+        }
+        
+        public string Model
+        {
+            get { return this.model; }
+            private set { this.model = value; }
+        }
+        
+        public int HorsePower
+        {
+            get { return this.horsePower; }
+            private set { this.horsePower = value; }
+        }
+        
+        public string RegistrationNumber
+        {
+            get { return this.registrationNumber; }
+            private set { this.registrationNumber = value; }
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"Make: {this.Make}")
+                .AppendLine($"Model: {this.Model}")
+                .AppendLine($"HorsePower: {this.HorsePower}")
+                .Append($"RegistrationNumber: {this.RegistrationNumber}");
+            return sb.ToString().Trim();
+        }
+
+    }
+}
