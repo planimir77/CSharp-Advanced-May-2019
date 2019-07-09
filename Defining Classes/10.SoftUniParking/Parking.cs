0a51743fb0faade3fbe91fdd48d25d2add837b67
planimir77 <34979738+planimir77@users.noreply.github.com>
2019-07-09 12:30:01 +0300

    Create Parking.cs

---
+++ Defining Classes/10.SoftUniParking/Parking.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftUniParking
+{
+    public class Parking
+    {
+        private List<Car> cars;
+        private int capacity;
+
+        public Parking(int capacity)
+        {
+            this.capacity = capacity;
+            this.cars = new List<Car>();
+        }
+
+        public int Count => this.cars.Count;
+
+        public string AddCar(Car car)
+        {
+            if (this.cars.Any(x => x.RegistrationNumber == car.RegistrationNumber))
+            {
+                return "Car with that registration number, already exists!";
+            }
+            else if (this.cars.Count == this.capacity)
+            {
+                return $"Parking is full!";
+            }
+            else
+            {
+                this.cars.Add(car);
+                return $"Successfully added new car {car.Make} {car.RegistrationNumber}";
+            }
+
+        }
+
+        public Car GetCar(string registrationNumber)
+        {
+            return this.cars.FirstOrDefault(x => x.RegistrationNumber == registrationNumber);
+        }
+
+        public string RemoveCar(string registrationNumber)
+        {
+            var car = this.cars.FirstOrDefault(x => x.RegistrationNumber == registrationNumber);
+            bool isCarExist = this.cars.Contains(car);
+            if (isCarExist)
+            {
+                this.cars.Remove(car);
+                return $"Successfully removed {registrationNumber}";
+            }
+            else
+            {
+                return $"Car with that registration number, doesn't exist!";
+            }
+        }
+        public void RemoveSetOfRegistrationNumber(List<string> registrationNumbers)
+        {
+            foreach (var registrationNumber in registrationNumbers)
+            {
+                this.cars.RemoveAll(x => x.RegistrationNumber == registrationNumber);
+            }
+        }
+    }
+}
