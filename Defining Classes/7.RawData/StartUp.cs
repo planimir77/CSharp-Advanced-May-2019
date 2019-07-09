17dd530d1705a296fbdf8ff9888dd31f3350243d
planimir77 <34979738+planimir77@users.noreply.github.com>
2019-07-09 12:31:55 +0300

    Create StartUp.cs

---
+++ Defining Classes/7.RawData/StartUp.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int numberOfCars = int.Parse(Console.ReadLine());
+
+            var cars = new Car[numberOfCars];
+
+            for (int i = 0; i < numberOfCars; i++)
+            {
+                var carInfo = Console.ReadLine()
+                .Split();
+                Car car = new Car(carInfo);
+                cars[i] = car;
+            }
+
+            string cargoType = Console.ReadLine();
+            foreach (var car in cars.Where(x => x.Cargo.CargoType == cargoType))
+            {
+                if (cargoType == "flamable")
+                {
+                    if (car.Engine.Power>250)
+                    {
+                        Console.WriteLine(car.Model.ToString());
+                    }
+                    
+                }
+                else if (cargoType == "fragile")
+                {
+                    if (car.Tires.TirePressure.FirstOrDefault() > 1)
+                    {
+                        continue;
+                    }
+                    Console.WriteLine(car.Model.ToString());
+                }
+            }
+            
+            //if (cargoType == "fragile" )
+            //{
+            //    foreach (var car in cars)
+            //    {
+            //        if (car.Tires.TirePressure.FirstOrDefault()>1)
+            //        {
+            //            continue;
+            //        }
+            //        Console.WriteLine(car.Model.ToString());
+            //    }
+            //}
+        }
+    }
+}
