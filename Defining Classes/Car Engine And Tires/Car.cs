f5ebeac826e40f0aafb84d776c431b785880a3fe
planimir77 <34979738+planimir77@users.noreply.github.com>
2019-07-09 12:34:35 +0300

    Create Car.cs

---
+++ Defining Classes/Car Engine And Tires/Car.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+    public class Car
+    {
+        private string make;
+        private string model;
+        private int year;
+        private double fuelQuantity;
+        private double fuelConsumption;
+        private Engine engine;
+        private Tire tire;
+
+
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int Year { get; set; }
+
+        public double FuelQuantity { get; set; }
+
+        public double FuelConsumption { get; set; }
+
+        public Engine Engine { get; set; }
+
+        public Tire Tire { get; set; }
+
+        public Tire[] Tires { get; }
+
+
+
+        public Car()
+        {
+
+        }
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption)
+            : this()
+        {
+            this.Make = make;
+            this.Model = model;
+            this.Year = year;
+            this.FuelQuantity = fuelQuantity;
+            this.FuelConsumption = FuelConsumption;
+        }
+
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption, Engine engine, Tire[] tires)
+            : this(make, model, year, fuelQuantity, fuelConsumption)
+        {
+            this.Engine = engine;
+            this.Tires = tires;
+        }
+
+    }
+}
